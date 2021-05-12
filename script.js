f5398661dc14fd070276b5f60a9d9cbd7c4f398f
IgorRoc <igor_roc@hotmail.com.br>
2021-05-12 16:08:43 -0300

    [add]: RTX toggle [add]: Neon toggle

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let speed = 0.7
 let zoom = 70
-const vel = document.getElementById('vel')
-const zoo = document.getElementById('zoo')
+let shadow = true
+let neon = false
+const vel = document.getElementById('vel').children[1]
+const zoo = document.getElementById('zoo').children[1]
+const rtx = document.getElementById('rtx').children[1]
+const neo = document.getElementById('neo').children[1]
 
 function changeSpeed(value) {
     speed += value;
@@ -9,7 +13,7 @@ function changeSpeed(value) {
     speed = Math.min(Math.max(.1, speed), 20);
 
     document.documentElement.style.setProperty('--ballBounce', `${speed}s`);
-    vel.children[1].innerText = `Velocidade: ${speed.toFixed(2)}s`
+    vel.innerText = `Velocidade: ${speed.toFixed(2)}s`
 }
 
 function changeZoom(value) {
@@ -18,5 +22,45 @@ function changeZoom(value) {
     zoom = Math.min(Math.max(1, zoom), 200);
 
     document.documentElement.style.setProperty('--fontSize', `${zoom}px`);
-    zoo.children[1].innerText = `Zoom: ${zoom}px`
+    zoo.innerText = `Zoom: ${zoom}px`
+}
+
+function toggleRTX() {
+    shadow = !shadow
+
+    let chao = document.getElementById('chao')
+    chao.classList.toggle('floor-light')
+
+    let bola = document.getElementById('bola')
+    bola.classList.toggle('ball-light')
+    
+    let sombra_bola = document.getElementById('sombra-bola')
+    sombra_bola.classList.toggle('ballShadow')
+
+    let cubo = document.getElementById('cubo')
+    for (const face of cubo.children) {
+        if(face.classList.contains("bottom")){
+            face.classList.toggle("bottom-shadow")
+        }else{
+            face.classList.toggle("side-shadow")
+        }
+    }
+
+
+    rtx.innerText = `RTX: ${shadow?"ON":"OFF"}`
+}
+
+function toggleNeon() {
+    neon = !neon
+
+    let bola = document.getElementById('bola')
+    bola.classList.toggle('ball-neon')
+    
+    let cubo = document.getElementById('cubo')
+    for (const face of cubo.children) {
+        face.classList.toggle("cube-neon")
+    }
+
+
+    neo.innerText = `Neon: ${neon?"ON":"OFF"}`
 }
